8978e5d45e837773b057a558a428a352f9b8484d
leejunnyeop <daffodil8620@naver.com>
2025-07-30 17:37:55 +0900

    api-server ci/cd 구축

---
+++ api-server/src/main/java/com/example/demo/testController/TestController.java
@@ -0,0 +1,13 @@
+package com.example.demo.testController;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+
+    @GetMapping("/api-server")
+    public String test(){
+        return "api-server 모듈 테스트";
+    }
+}
