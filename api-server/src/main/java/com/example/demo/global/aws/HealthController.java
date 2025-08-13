176b570966c1eda7c790371989effd01484d9a27
leejunnyeop <daffodil8620@naver.com>
2025-08-13 16:20:47 +0900

    api-server ci/cd 구축-62

--- api-server/src/main/java/com/example/demo/global/aws/HealthController.java
+++ api-server/src/main/java/com/example/demo/global/aws/HealthController.java
@@ -1,4 +1,4 @@
-package com.example.demo.testController;
+package com.example.demo.global.aws;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
