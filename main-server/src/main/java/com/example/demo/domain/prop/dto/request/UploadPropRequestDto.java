38821f799e68c6ceb2271eefc346d4a9f66b8ddb
gzhan0226 <gzhan@naver.com>
2025-08-14 17:32:34 +0900

    refactor : 엔티티 변경에 따른 수정
    # Conflicts:
    #	main-server/src/main/java/com/example/demo/domain/prop/controller/PropController.java
    #	main-server/src/main/java/com/example/demo/domain/prop/dto/request/UploadPropRequestDto.java›
    #	main-server/src/main/java/com/example/demo/domain/prop/repository/PropRepository.java
    #	main-server/src/main/java/com/example/demo/domain/user/repository/UsersRepository.java

--- main-server/src/main/java/com/example/demo/domain/prop/dto/request/UploadPropRequestDto.java
+++ main-server/src/main/java/com/example/demo/domain/prop/dto/request/UploadPropRequestDto.java
@@ -1,8 +1,6 @@
 package com.example.demo.domain.prop.dto.request;
 
 import com.example.demo.domain.validation.annotation.NotEmptyFile;
-import jakarta.validation.constraints.NotNull;
-import lombok.Builder;
 import org.springframework.web.multipart.MultipartFile;
 
 public record UploadPropRequestDto(@NotEmptyFile MultipartFile file) {
--- main-server/src/main/java/com/example/demo/domain/user/repository/UsersRepository.java
+++ main-server/src/main/java/com/example/demo/domain/user/repository/UsersRepository.java
@@ -1,10 +1,7 @@
 package com.example.demo.domain.user.repository;
 
 import com.example.demo.domain.user.entity.Users;
-import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UsersRepository extends JpaRepository<Users, String> {
-
-    Optional<Users> findById(String username);
 }
