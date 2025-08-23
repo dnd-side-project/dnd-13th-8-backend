041032d2863e1f485d8ba1dd7754609e08420f07
leejunnyeop <daffodil8620@naver.com>
2025-08-24 01:39:47 +0900

    swagger 문서화

---
+++ main-server/src/main/java/com/example/demo/global/swagger/SwaggerConfig.java
@@ -0,0 +1,19 @@
+package com.example.demo.global.swagger;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    public OpenAPI mainServerOpenAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("Main Server API")
+                        .description("메인 서버 문서")
+                        .version("v1"));
+    }
+}
