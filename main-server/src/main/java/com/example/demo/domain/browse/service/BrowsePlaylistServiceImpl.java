12f4d85c1243ffb53ca09992230ac07a8e40dc44
leejunnyeop <daffodil8620@naver.com>
2025-08-27 17:56:34 +0900

    refactor(둘러보기) : 신규회원일 때 랜덤으로 플레이리스트 기능 개선

--- main-server/src/main/java/com/example/demo/domain/browse/service/BrowsePlaylistServiceImpl.java
+++ main-server/src/main/java/com/example/demo/domain/browse/service/BrowsePlaylistServiceImpl.java
@@ -13,11 +13,16 @@ import com.example.demo.domain.recommendation.entity.UserPlaylistHistory;
 import com.example.demo.domain.recommendation.repository.UserPlaylistHistoryRepository;
 import com.example.demo.domain.user.entity.Users;
 import com.example.demo.domain.user.repository.UsersRepository;
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
+import java.util.Random;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+@Slf4j
 @RequiredArgsConstructor
 @Service
 @Transactional
@@ -29,16 +34,39 @@ public class BrowsePlaylistServiceImpl implements BrowsePlaylistService {
     private final PlaylistRepository playlistRepository;
     private final BrowseViewCountService browseViewCountService;
 
-    // 구현
     @Override
     public BrowseResponse getShuffledPlaylists(String userId) {
         List<BrowsePlaylistCard> cards = browsePlaylistRepository.findByUserIdOrderByPosition(userId);
+
+        if (cards.isEmpty()) {
+            log.info("셔플 카드 없음 → fallback 적용: userId={}", userId);
+
+            // 1~5번 ID 셔플
+            List<Long> fallbackIds = new ArrayList<>(List.of(1L, 2L, 3L, 4L, 5L));
+            Collections.shuffle(fallbackIds, new Random(System.nanoTime()));
+
+            // fallback 리스트
+            List<BrowsePlaylistDto> fallbackDtos = new ArrayList<>();
+
+            for (Long browsePlayId : fallbackIds) {
+                browsePlaylistRepository.findById(browsePlayId).ifPresentOrElse(
+                        browse -> fallbackDtos.add(BrowsePlaylistDto.from(browse)), // 변환 및 저장
+                        () -> log.warn(" fallback playlistId={} 없음. 스킵", browsePlayId)
+                );
+            }
+
+            return new BrowseResponse(fallbackDtos);
+        }
+
+        // 일반 셔플 카드 반환
         List<BrowsePlaylistDto> dtos = cards.stream()
                 .map(BrowsePlaylistDto::from)
                 .toList();
 
         return new BrowseResponse(dtos);
     }
+
+
 
     @Override
     public void confirmAndLogPlayback(String id, Long playlistId) {
