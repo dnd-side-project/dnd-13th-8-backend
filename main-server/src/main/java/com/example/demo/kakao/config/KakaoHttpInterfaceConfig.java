802f678bfc9a711bb1add9617f7e2f9c00f6b35a
leejunnyeop <daffodil8620@naver.com>
2025-08-30 04:39:22 +0900

    카카오로그인 로그 추적 메세지 컨버터 추가

--- main-server/src/main/java/com/example/demo/kakao/config/KakaoHttpInterfaceConfig.java
+++ main-server/src/main/java/com/example/demo/kakao/config/KakaoHttpInterfaceConfig.java
@@ -3,9 +3,13 @@ package com.example.demo.kakao.config;
 import com.example.demo.kakao.controller.KakaoApiHttp;
 import com.example.demo.kakao.controller.KakaoAuthHttp;
 import java.time.Duration;
+import java.util.ArrayList;
+import java.util.List;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.client.SimpleClientHttpRequestFactory;
+import org.springframework.http.converter.FormHttpMessageConverter;
+import org.springframework.http.converter.HttpMessageConverter;
 import org.springframework.web.client.RestClient;
 import org.springframework.web.client.support.RestClientAdapter;
 import org.springframework.web.service.invoker.HttpServiceProxyFactory;
@@ -18,9 +22,13 @@ public class KakaoHttpInterfaceConfig {
         httpRequestFactory.setConnectTimeout((int) Duration.ofSeconds(2).toMillis());
         httpRequestFactory.setReadTimeout((int) Duration.ofSeconds(3).toMillis());
 
+        List<HttpMessageConverter<?>> converters = new ArrayList<>();
+        converters.add(new FormHttpMessageConverter());
+
         return RestClient.builder()
                 .baseUrl(baseUrl)
                 .requestFactory(httpRequestFactory)
+                .messageConverters(converters)
                 .build();
     }
 
