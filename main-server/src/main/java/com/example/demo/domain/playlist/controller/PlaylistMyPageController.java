8d417deb8122ac6121f0a035400b1dad583e471e
leejunnyeop <daffodil8620@naver.com>
2025-08-29 17:29:21 +0900

    chore(마이페이지/controller) : 코드정리

--- main-server/src/main/java/com/example/demo/domain/playlist/controller/PlaylistMyPageController.java
+++ main-server/src/main/java/com/example/demo/domain/playlist/controller/PlaylistMyPageController.java
@@ -25,12 +25,11 @@ import jakarta.validation.Valid;
 import java.io.IOException;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 
-@Slf4j
+
 @RestController
 @RequestMapping("/main/mypage/playlists")
 @RequiredArgsConstructor
