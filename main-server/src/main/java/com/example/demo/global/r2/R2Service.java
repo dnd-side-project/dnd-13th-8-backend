ee3b349da15b84797306e1d66275089aa0fc6c41
gzhan0226 <gzhan@naver.com>
2025-09-07 01:03:39 +0900

    refactor(R2): 버킷 하나로 통합

--- main-server/src/main/java/com/example/demo/global/r2/R2Service.java
+++ main-server/src/main/java/com/example/demo/global/r2/R2Service.java
@@ -6,13 +6,9 @@ import org.springframework.stereotype.Service;
 import software.amazon.awssdk.core.sync.RequestBody;
 import software.amazon.awssdk.services.s3.S3Client;
 import software.amazon.awssdk.services.s3.model.DeleteObjectRequest;
-import software.amazon.awssdk.services.s3.model.GetObjectRequest;
 import software.amazon.awssdk.services.s3.model.PutObjectRequest;
 import software.amazon.awssdk.services.s3.presigner.S3Presigner;
-import software.amazon.awssdk.services.s3.presigner.model.GetObjectPresignRequest;
-import software.amazon.awssdk.services.s3.presigner.model.PresignedGetObjectRequest;
 
-import java.time.Duration;
 import java.util.UUID;
 
 @Service
