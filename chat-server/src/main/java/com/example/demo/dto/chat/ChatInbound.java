92ced575d0dd0d323ff2d14ad2cca2beaac48120
gzhan0226 <gzhan@naver.com>
2026-01-20 22:22:02 +0900

    refactor(Chat): DTO 정리

--- chat-server/src/main/java/com/example/demo/dto/chat/ChatInbound.java
+++ chat-server/src/main/java/com/example/demo/dto/chat/ChatInbound.java
@@ -1,18 +1,16 @@
-package com.example.demo.dto;
+package com.example.demo.dto.chat;
 
 import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Getter
 @NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class ChatInbound {
-    @NotBlank
-    private String senderId;
-
-    @NotBlank
-    private String username;
-
     @NotBlank
     private String content;
 
