45e4485d1e2735468c87eee98ee80106bcec967a
gzhan0226 <gzhan@naver.com>
2026-04-10 00:42:38 +0900

    refactor(PlaylistService): 함수형 프로그래밍 방식으로 리팩토링, 책임 분리

--- main-server/src/main/java/com/example/demo/domain/playlist/service/common/PlaylistSaveService.java
+++ main-server/src/main/java/com/example/demo/domain/playlist/service/common/PlaylistSaveService.java
@@ -4,7 +4,6 @@ import com.example.demo.domain.cd.dto.request.SaveCdRequest;
 import com.example.demo.domain.playlist.dto.save.PlaylistDraft;
 import com.example.demo.domain.playlist.dto.save.SavePlaylistRequest;
 import com.example.demo.domain.playlist.dto.save.SavePlaylistResponse;
-import com.example.demo.domain.playlist.entity.Playlist;
 
 public interface PlaylistSaveService {
     String createDraft(SavePlaylistRequest savePlaylistRequest, SaveCdRequest saveCdRequest);
